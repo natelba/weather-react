5d43def03bfccb019e08ef76cd96a77dc36adda2
natelba <77124289+natelba@users.noreply.github.com>
2021-06-03 14:46:16 -0400

    moving vanilla to react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 import axios from "axios";
 
@@ -6,10 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Weather Search App</h1>
+        <WeatherSearch />
         <a
           className="App-link"
           href="https://reactjs.org"
